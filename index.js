e548ca02a6d89a79a56dd2e90733ded1d8c2b233
git-aftab <mdaftab0726@gmail.com>
2026-01-13 14:52:57 +0530

    setting up the project

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: "./.env"
+})
+
+
+let userName = process.env.name
+let db = process.env.db
+console.log('name = ',userName);
+console.log("db=",db)
 console.log("hello world");
