baf81934c086637afea4285cdedc7f0ad77ffb1d
git-aftab <mdaftab0726@gmail.com>
2026-01-17 12:25:10 +0530

    Setting up the Access Token and Refresh Token for Seemless user Auth

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
   {
@@ -67,6 +68,32 @@ userSchema.pre("save", async function (next) {
 });
 
 userSchema.methods.isPasswordCorrect = async function (password) {
-    return await bcrypt.compare(password, this.password)
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    // below is the payload
+    {
+      _id: this._id, //id from mongoDB database.
+      email: this.email,
+      username: this.username,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY },
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      //in refresh token no need much payload. Good practice.
+      // email: this.email,
+      // username: this.username,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY },
+  );
 };
 export const User = mongoose.model("user", userSchema);
