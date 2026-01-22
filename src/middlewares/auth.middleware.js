a653a1751cd05ca61756783d4d495784ef14ed1c
git-aftab <mdaftab0726@gmail.com>
2026-01-22 07:49:03 +0530

    completed setting up auth.middleware.js to decode accessToken and inject in the req to make verification seemless wtih controlleres

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,27 @@
+import { User } from "../models/user.model";
+import { ApiError } from "../utils/api-error";
+import { asyncHandler } from "../utils/asyncHandler";
+import jwt from "jsonwebtoken";
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+  const token =
+    req.cookies?.accessToken ||
+    req.header("Authorization")?.replace("Bearer", "");
+
+  if (!token) {
+    throw new ApiError(401, "Unauthorized request");
+  }
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken -emailVerificatonToken -emailVerificationExpiry", // dont wanna send these data
+    );
+
+    if (!user) {
+      throw new ApiError(401, "Invalid access token");
+    }
+  } catch (error) {
+    throw new ApiError(401, "Invalid access token", error);
+  }
+});
