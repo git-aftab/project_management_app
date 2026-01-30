1251ab6bf33abfab8b2c9ab3844842173f8c582b
git-aftab <mdaftab0726@gmail.com>
2026-01-30 23:18:56 +0530

    Completed the task.model.js 'Task Schema'

---
+++ src/models/task.models.js
@@ -0,0 +1,46 @@
+import mongoose, { Schema } from "mongoose";
+import { AvailableTaskStatues, TaskStatusEnum } from "../utils/constants.js";
+import { assign } from "nodemailer/lib/shared";
+
+const TaskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      Project: {
+        type: Schema.Types.ObjectId,
+        ref: "Project",
+        required: true,
+      },
+      assignedTo: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+      assignedBy: {
+        Type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+      status: {
+        type: String,
+        enum: AvailableTaskStatues,
+        default: TaskStatusEnum.TODO,
+      },
+      attachments: {
+        type: [
+          {
+            url: String,
+            mimetype: String,
+            size: Number,
+          },
+        ],
+        default: [],
+      },
+    },
+  },
+  { timestamps: true },
+);
+
+export const Tasks = mongoose.model("Tasks", TaskSchema);
