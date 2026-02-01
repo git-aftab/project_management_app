026664693dba366713c756d104eb6be205f9eb1b
git-aftab <mdaftab0726@gmail.com>
2026-02-01 23:31:50 +0530

    filler

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -1,8 +1,43 @@
 import { User } from "../models/user.model.js";
-import { project } from "../models/project.models.js";
+import { Project } from "../models/project.models.js";
 import { projectNotes } from "../models/note.models.js";
-import { projectMember } from "../models/projectmember.models.js";
+import { ProjectMember } from "../models/projectmember.models.js";
 import { ApiResponse } from "../utils/api-response.js";
 import { ApiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
+const getProjects = asyncHandler(async (req, res) => {
+  //test
+});
+
+const getProjectById = asyncHandler(async (req, res) => {
+  //test
+});
+
+const createProject = asyncHandler(async (req, res) => {
+  //test
+});
+
+const updateProject = asyncHandler(async (req, res) => {
+  //test
+});
+
+const deleteProject = asyncHandler(async (req, res) => {
+  //test
+});
+
+const getProjectMembers = asyncHandler(async (req, res) => {
+  //test
+});
+
+const addMembersToProject = asyncHandler(async (req, res) => {
+  //test
+});
+
+const deleteProjectMember = asyncHandler(async (req, res) => {
+  //test
+});
+
+const updateMemberRole = asyncHandler(async (req, res) => {
+  //test
+});
