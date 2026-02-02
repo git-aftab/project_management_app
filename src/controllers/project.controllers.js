32aeaaff889a471a9dd3332e9ead3dcb85e20d47
git-aftab <mdaftab0726@gmail.com>
2026-02-02 23:28:00 +0530

    completed the logic of Creating and updating Project

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -5,6 +5,8 @@ import { ProjectMember } from "../models/projectmember.models.js";
 import { ApiResponse } from "../utils/api-response.js";
 import { ApiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import mongoose from "mongoose";
+import { UserRolesEnum } from "../utils/constants.js";
 
 const getProjects = asyncHandler(async (req, res) => {
   //test
@@ -15,11 +17,44 @@ const getProjectById = asyncHandler(async (req, res) => {
 });
 
 const createProject = asyncHandler(async (req, res) => {
-  //test
+  const { name, description } = req.body;
+  const project = await Project.create({
+    name,
+    description,
+    createdBy: new mongoose.Types.ObjectId(req.user._id),
+  });
+
+  await ProjectMember.create({
+    user: new mongoose.Types.ObjectId(req.user._id),
+    project: new mongoose.Types.ObjectId(project._id),
+    role: UserRolesEnum.ADMIN,
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, project, "Project Created Successfully"));
 });
 
 const updateProject = asyncHandler(async (req, res) => {
-  //test
+  const { name, description } = req.body;
+  const { projectId } = req.params;
+
+  const project = await Project.findByIdAndUpdate(
+    projectId,
+    {
+      name,
+      description,
+    },
+    { new: true },
+  );
+
+  if (!project) {
+    throw new ApiError(404, "Project now found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, project, "Project updated Successfully"));
 });
 
 const deleteProject = asyncHandler(async (req, res) => {
@@ -41,3 +76,15 @@ const deleteProjectMember = asyncHandler(async (req, res) => {
 const updateMemberRole = asyncHandler(async (req, res) => {
   //test
 });
+
+export {
+  getProjects,
+  getProjectById,
+  getProjectMembers,
+  createProject,
+  updateProject,
+  updateMemberRole,
+  deleteProject,
+  deleteProjectMember,
+  addMembersToProject,
+};
