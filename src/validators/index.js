34282db99d4fee26e23ce9a9e90640449eece622
git-aftab <mdaftab0726@gmail.com>
2026-02-07 23:55:26 +0530

    added validators for creating project and adding member to project

--- src/validators/index.js
+++ src/validators/index.js
@@ -1,4 +1,5 @@
 import { body } from "express-validator";
+import { AvailableUserRole } from "../utils/constants";
 
 const userRegisterValidator = () => {
   return [
@@ -55,10 +56,35 @@ const userForgotPasswordValidator = () => {
 const userResetForgotPasswordValidator = () => {
   return [body("newPassword").notEmpty().withMessage("Password is required.")];
 };
+
+const createProjectValidator = () => {
+  return [
+    body("name").notEmpty().withMessage("Name is required"),
+    body("description").optional(),
+  ];
+};
+
+const addMemberToProjectValidator = () => {
+  return [
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Email is invalid"),
+    body("role")
+      .notEmpty()
+      .withMessage("Role is required")
+      .isIn(AvailableUserRole)
+      .withMessage("Role is invalid"),
+  ];
+};
 export {
   userRegisterValidator,
   userLoginValidator,
   userChangeCurrentPasswordValidator,
   userForgotPasswordValidator,
-  userResetForgotPasswordValidator
+  userResetForgotPasswordValidator,
+  createProjectValidator,
+  addMemberToProjectValidator
 };
