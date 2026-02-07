379fab57abbf2239c7dd79905e8d95297d3c588e
git-aftab <mdaftab0726@gmail.com>
2026-02-07 23:46:29 +0530

    Completed validateProjectPermission() so that only admin can modify and edit projects

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,6 +2,8 @@ import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
+import { ProjectMember } from "../models/projectmember.models.js";
+import mongoose from "mongoose";
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
   const token =
@@ -28,3 +30,34 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
     throw new ApiError(401, "Invalid access token", error);
   }
 });
+
+export const validateProjectPermission = (roles = []) => {
+  asyncHandler(async (req, res, next) => {
+    const { projectId } = req.params;
+
+    if (!projectId) {
+      throw new ApiError(404, "Project id is missing");
+    }
+
+    // assuming this runs only after verifyJWT
+    const project = await ProjectMember.findOne({
+      project: new mongoose.Types.ObjectId(projectId),
+      user: new mongoose.Types.ObjectId(req.user._id),
+    });
+
+    if (!project) {
+      throw new ApiError(404, "Project not found");
+    }
+
+    // not getting role from frontend insetead access form DB because user can give any other role.
+    const givenRole = project?.role;
+    req.user.role = givenRole;
+
+    if (!roles.includes(givenRole)) {
+      throw new ApiError(403, "You Dont have access to perform this action");
+    }
+
+    // if correct
+    next();
+  });
+};
