6664ef67c2f204926f526b959245c99d3d43e907
git-aftab <mdaftab0726@gmail.com>
2026-02-16 23:27:11 +0530

    Completed Create Task function

--- src/controllers/task.controllers.js
+++ src/controllers/task.controllers.js
@@ -6,14 +6,46 @@ import { ApiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import mongoose from "mongoose";
 import { AvailableUserRole, UserRolesEnum } from "../utils/constants.js";
-import { Task } from "../models/task.models.js";
+import { Task, Tasks } from "../models/task.models.js";
 import { subTask } from "../models/subtask.models.js";
 
 const getTasks = asyncHandler(async (req, res) => {
   // TASk
 });
 const createTasks = asyncHandler(async (req, res) => {
-  // TASk
+  const { title, description, assignedTo, status } = req.body;
+  const { ProjectId } = req.params;
+  const project = await Project.findById(ProjectId);
+
+  if (!project) {
+    throw new ApiError(404, "Project Not Found");
+  }
+  // we will also be getting a file, I can also be an array of file
+  const files = req.files || [];
+
+  const attachments = files.map((file) => {
+    return {
+      url: `${process.env.SERVER_URL}/images/${file.originalname}`,
+      mimetype: file.mimetype,
+      size: file.size,
+    };
+  });
+
+  const task = await Tasks.create({
+    title,
+    description,
+    project: new mongoose.Types.ObjectId(ProjectId),
+    assignedTo: assignedTo
+      ? new mongoose.Types.ObjectId(assignedTo)
+      : undefined,
+    status,
+    assingedBy: new mongoose.Types.ObjectId(req.user._id),
+    attachments,
+  });
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, task, "Task Created Successfully"));
 });
 const UpdateTasks = asyncHandler(async (req, res) => {
   // TASk
@@ -34,14 +66,13 @@ const deleteSubTasks = asyncHandler(async (req, res) => {
   // TASk
 });
 
-
 export {
-    createTasks,
-    createSubTasks,
-    getTasks,
-    getTasksById,
-    deleteSubTasks,
-    deleteTasks,
-    updateSubTasks,
-    UpdateTasks
-}
+  createTasks,
+  createSubTasks,
+  getTasks,
+  getTasksById,
+  deleteSubTasks,
+  deleteTasks,
+  updateSubTasks,
+  UpdateTasks,
+};
