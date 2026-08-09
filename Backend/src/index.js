5e0a079e700f74ed5073edfafb6be68126457e69
aftab <mdaftab0726@gmail.com>
2026-08-09 15:57:47 +0530

    Added graceful shutdown to safely exist the server without breaking ongoing uploads or works

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,20 +1,73 @@
 import dotenv from "dotenv";
 import app from "./app.js";
 import connectDB from "./db/db.js";
+import mongoose from "mongoose";
+import redis from "./config/redis.js";
 
 dotenv.config({
   path: "./.env",
 });
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-connectDB()
-  .then(() => {
-    app.listen(port, () => {
-      console.log(`app is listenting on http://localhost:${port}`);
+let server;
+
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    server = app.listen(PORT, () => {
+      console.log(`app is listening on http://localhost:${PORT}`);
     });
-  })
-  .catch((err) => {
+  } catch (err) {
     console.error("MongoDB connection error", err);
     process.exit(1);
-  });
+  }
+};
+
+const gracefulShutdown = async (signal) => {
+  console.log(`Received ${signal}. Closing server...`);
+
+  // Stop accepting new connections
+  try {
+    if (server) {
+      await new Promise((resolve, reject) => {
+        server.close((err) => {
+          if (err) reject(err);
+          else resolve();
+        });
+      });
+      console.log("Server Closed.")
+    }
+
+    await mongoose.connection.close();
+    console.log("MongoDB connection closed.");
+
+    // Close Redis
+    await redis.quit();
+    console.log("Redis connection closed.");
+
+    console.log("Graceful shutdown completed.");
+    
+    process.exit(0);
+  } catch (error) {
+    console.log("Error during graceful Shutdown:", error);
+    process.exit(1);
+  }
+};
+
+process.on("SIGTERM", () => gracefulShutdown("SIGTERM"));
+process.on("SIGINT", () => gracefulShutdown("SIGINT"));
+
+startServer();
+
+// connectDB()
+//   .then(() => {
+//     app.listen(port, () => {
+//       console.log(`app is listenting on http://localhost:${port}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.error("MongoDB connection error", err);
+//     process.exit(1);
+//   });
